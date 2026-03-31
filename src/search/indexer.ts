9502da315cbc116810a4e4bae333b5949bcf6976
RetoWen <reto@rready.com>
2026-03-31 19:47:53 +0200

    feat: add incremental delta indexing

--- src/search/indexer.ts
+++ src/search/indexer.ts
@@ -3,3 +3,7 @@
 export async function bulkIndex(documents: any[], batchSize = 500) {
   // process in batches for memory efficiency
 }
+// Incremental update handler
+export async function indexDelta(since: Date) {
+  // index only documents modified since last run
+}
